7c8d3503b9eb7e005e8b0877f471411aeaefe0a5
stefanofreyr <ste_cast@hotmail.it>
2024-10-05 11:07:30 +0100

    Implement "Add" feature

--- web.py
+++ web.py
@@ -1,15 +1,23 @@
 import streamlit as st
 import functions
 
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
 todos = functions.get_todos()
 
 st.title("The ***Really Cool*** Todo App")
 st.subheader("Made by Stefano Freyr Castiglione")
 
 for todo in todos:
-    st.checkbox(todo)
+    st.checkbox(todo, on_change=)
 
 add_button = st.button(label="Add",icon=":material/add:",use_container_width=True)
 
 if add_button == True:
-    st.text_input(label="",placeholder="Add a todo:")
+    st.text_input(label="",placeholder="Add a todo",
+                  on_change=add_todo,key="new_todo")
+
+
